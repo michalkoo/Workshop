08dc8db57cd130376524f1e9988dd7355285f2fa
michalkoo <michal.koziarowski@gmail.com>
2016-03-08 11:26:43 +0100

    Search properties added to SearchParameters class

--- LogSearch/src/LogSearch/Models/SearchParameters.cs
+++ LogSearch/src/LogSearch/Models/SearchParameters.cs
@@ -1,6 +1,16 @@
-﻿namespace LogSearch.ParserHelper
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogSearch.ParserHelper
 {
     public class SearchParameters
     {
+        public string SearchPhrase { get; set; }
+
+        public DateTime DateFrom { get; set; }
+
+        public DateTime DateTo { get; set; }
+
+        public List<string> Servers { get; set; }
     }
 }
--- LogSearch/src/LogSearch/Services/LogSearchService.cs
+++ LogSearch/src/LogSearch/Services/LogSearchService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogSearch.ParserHelper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,11 @@ namespace LogSearch.Services
 {
     public class LogSearchService
     {
+        private SearchParameters _searchParameters;
 
+        public LogSearchService(SearchParameters parameters)
+        {
+            _searchParameters = parameters;
+        }
     }
 }
